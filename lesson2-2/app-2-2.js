98bf393f77157a0c7f5b487c43c30ce214584d95
Igor-Dev-Srx <scriptex88@gmail.com>
2024-05-03 13:42:08 +0200

    remove folder

--- lesson2-2/app-2-2.js
+++
@@ -1,6 +0,0 @@
-let titleProject = "Оценка стоимости";
-let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
-let screenPrice = 3800;
-let percentage = 22;
-let fullPrice = 2100;
-let responsive = true;
