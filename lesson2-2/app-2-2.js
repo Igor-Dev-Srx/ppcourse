8a8fb730948f193092c070a23d2c685504417743
Igor-Dev-Srx <scriptex88@gmail.com>
2024-05-03 13:31:22 +0200

    add folder lesson2-2

---
+++ lesson2-2/app-2-2.js
@@ -0,0 +1,6 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 3800;
+let percentage = 22;
+let fullPrice = 2100;
+let responsive = true;
