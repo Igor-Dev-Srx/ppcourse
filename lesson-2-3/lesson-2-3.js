0761ccdc293aa1bd0f9fd0471bb32cb9e83afa36
Igor-Dev-Srx <scriptex88@gmail.com>
2024-05-04 15:35:13 +0200

    fixed lesson-2-3.js

--- lesson-2-3/lesson-2-3.js
+++ lesson-2-3/lesson-2-3.js
@@ -5,25 +5,24 @@
 // let fullPrice = 2100;
 // let responsive = true;
 
-// let titleProject = prompt("Название проекта?");
-// let screensValue = prompt("Тип экрана(шаблонные, с уникальным дизайном, с анимациями)");
-// let responsive = prompt("Нужен ли респонсивный сайт?");
-// let serviceOne = prompt("Какой сервис нужен?");
-// let servicePriceOne = prompt("Сколько это будет стоить?");
-// let serviceTwo = prompt("Какой еще сервис тебе нужен?");
-// let servicePriceTwo = prompt("Сколько будет стоить этот второй сервис?");
-// let screenPrice = 3800;
-// let percentage = 15;
+let titleProject = prompt("Название проекта?");
+let screensValue = prompt("Тип экрана(шаблонные, с уникальным дизайном, с анимациями)");
+let responsive = prompt("Нужен ли респонсивный сайт?");
+let serviceOne = prompt("Какой сервис нужен?");
+let servicePriceOne = prompt("Сколько это будет стоить?");
+let serviceTwo = prompt("Какой еще сервис тебе нужен?");
+let servicePriceTwo = prompt("Сколько будет стоить этот второй сервис?");
+let screenPrice = 3800;
+let percentage = 15;
 
-// let fullPrice = screenPrice + +servicePriceOne + +servicePriceTwo;
-// console.log(fullPrice, "Общая сумма");
+let fullPrice = screenPrice + +servicePriceOne + +servicePriceTwo;
+console.log(fullPrice, "Общая сумма");
 
-// let percentageResult = fullPrice * (percentage / 100);
-// console.log(percentageResult, "Сумма процента подрядчику");
+let percentageResult = fullPrice * (percentage / 100);
+console.log(percentageResult, "Сумма процента подрядчику");
 
-// let servicePercentPrice = fullPrice - percentageResult;
-// console.log(Math.round(servicePercentPrice), "Сумма в карман))");
-let fullPrice = -1;
+let servicePercentPrice = fullPrice - percentageResult;
+console.log(Math.round(servicePercentPrice), "Сумма в карман))");
 
   if (fullPrice >= 50000) {
     console.log("Скидка 10%");
